00802d3968fd92727356987d2d4929f5ad2b215d
Jan Gavlik <jan.gavlik@tieto.com>
2021-04-05 12:40:50 +0200

    Add tags

--- src/main/java/cz/fi/muni/pa165/esports/PersistenceSampleApplicationContext.java
+++ src/main/java/cz/fi/muni/pa165/esports/PersistenceSampleApplicationContext.java
@@ -5,15 +5,20 @@ import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories
 @ComponentScan(basePackages = "cz.fi.muni.pa165.esports.dao")
 public class PersistenceSampleApplicationContext {
+
 
     /**
      * Starts up a container that emulates behavior prescribed in JPA spec for container-managed EntityManager
