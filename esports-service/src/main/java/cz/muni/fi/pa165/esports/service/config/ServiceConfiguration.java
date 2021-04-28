af660d1ff671d19f6d31617a64034130af511772
Gabriela Kandová <gabika.kandova@gmail.com>
2021-04-28 20:25:56 +0200

    Added basic service configuration class as per the reference project

---
+++ esports-service/src/main/java/cz/muni/fi/pa165/esports/service/config/ServiceConfiguration.java
@@ -0,0 +1,25 @@
+package cz.muni.fi.pa165.esports.service.config;
+
+import cz.muni.fi.pa165.esports.PersistenceSampleApplicationContext;
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Service configuration class.
+ * Taken from the <a href="https://github.com/fi-muni/PA165/blob/master/eshop-service/src/main/java/cz/fi/muni/pa165/service/config/ServiceConfiguration.java">reference project</a>
+ *
+ * @author Gabriela Kandova
+ */
+@Configuration
+@Import(PersistenceSampleApplicationContext.class)
+//@ComponentScan?
+public class ServiceConfiguration {
+    @Bean
+    public Mapper dozer() {
+        DozerBeanMapper dozer = new DozerBeanMapper();
+        return dozer;
+    }
+}
