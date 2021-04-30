dc610a2d3e1585fdb74a0fcf20411b7ca6d3d4e6
Gabriela Kandová <gabika.kandova@gmail.com>
2021-04-30 23:12:56 +0200

    Changed PlayerDTO to use Lombok

--- esports-api/src/main/java/cz/muni/fi/pa165/esports/dto/PlayerDTO.java
+++ esports-api/src/main/java/cz/muni/fi/pa165/esports/dto/PlayerDTO.java
@@ -1,37 +1,24 @@
 package cz.muni.fi.pa165.esports.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 import java.util.Objects;
 
 /**
  * @author Radovan Tomasik
  */
+@Getter
+@Setter
+@NoArgsConstructor
 public class PlayerDTO {
     private Long id;
 
     private String Name;
 
     private Date joinedDate;
-
-    public PlayerDTO() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-
-    public Date getJoinedDate() {
-        return joinedDate;
-    }
-
-    public void setJoinedDate(Date joinedDate) {
-        this.joinedDate = joinedDate;
-    }
 
     @Override
     public boolean equals(Object o) {
