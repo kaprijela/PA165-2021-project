85962c3c6974063442284e081936263af99a24e4
Gabriela Kandová <gabika.kandova@gmail.com>
2021-04-30 18:02:27 +0200

    Fixed TeamFacade tests were expecting Competition

--- esports-service/src/test/java/cz/muni/fi/pa165/esports/service/facade/TeamFacadeTest.java
+++ esports-service/src/test/java/cz/muni/fi/pa165/esports/service/facade/TeamFacadeTest.java
@@ -1,28 +1,18 @@
 package cz.muni.fi.pa165.esports.service.facade;
 
-import cz.muni.fi.pa165.esports.dao.TeamDao;
 import cz.muni.fi.pa165.esports.dto.TeamDTO;
-import cz.muni.fi.pa165.esports.entity.Competition;
 import cz.muni.fi.pa165.esports.entity.Team;
-import cz.muni.fi.pa165.esports.facade.TeamFacade;
 import cz.muni.fi.pa165.esports.service.BeenMappingService;
 import cz.muni.fi.pa165.esports.service.TeamService;
-import cz.muni.fi.pa165.esports.service.config.ServiceConfiguration;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
-import org.mockito.*;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.annotations.BeforeClass;
 import org.mockito.InjectMocks;
-import java.util.ArrayList;
-import java.util.List;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.mockito.Mockito.*;
-import static org.testng.Assert.*;
 
 /**
  * @author Radovan Tomasik
@@ -81,8 +71,8 @@ public class TeamFacadeTest {
         menE.setId(1L);
         menE.setName("Men of Gondor");
         when(beanMappingService.mapTo(teamDTO, Team.class)).thenReturn(menE);
-        teamFacade.createTeam(teamDTO);
-        verify(beanMappingService, times(1)).mapTo(teamDTO, Competition.class);
+        teamFacade.registerNewTeam(teamDTO);
+        verify(beanMappingService, times(1)).mapTo(teamDTO, Team.class);
         verify(teamService, times(1)).create(menE);
 
     }
@@ -96,8 +86,8 @@ public class TeamFacadeTest {
         menE.setId(1L);
         menE.setName("Men of Gondor");
         when(beanMappingService.mapTo(teamDTO, Team.class)).thenReturn(menE);
-        teamFacade.createTeam(teamDTO);
-        verify(beanMappingService, times(1)).mapTo(teamDTO, Competition.class);
+        teamFacade.registerNewTeam(teamDTO);
+        verify(beanMappingService, times(1)).mapTo(teamDTO, Team.class);
         verify(teamService, times(1)).create(menE);
         teamFacade.removeTeam(teamDTO);
         verify(teamService, times(1)).remove(menE);
