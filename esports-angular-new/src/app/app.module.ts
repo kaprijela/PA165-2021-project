bc144fb0ccd687ad646be738cfce9e1615207f49
Gabriela Kandová <gabika.kandova@gmail.com>
2021-05-22 23:05:25 +0200

    Added responsive navbar to web app

--- esports-angular-new/src/app/app.module.ts
+++ esports-angular-new/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
+import {RouterModule} from "@angular/router";
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
@@ -8,17 +8,20 @@ import {PlayerService} from "./service/player.service";
 import {PlayerListComponent} from './components/player-list/player-list.component';
 import {CompetitionService} from "./service/competition.service";
 import {CompetitionListComponent} from './components/competition-list/competition-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerListComponent,
-    CompetitionListComponent
+    CompetitionListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   providers: [PlayerService, CompetitionService],
   bootstrap: [AppComponent]
