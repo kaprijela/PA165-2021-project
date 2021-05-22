08df454b71da08d1ca59103db102b04ae8bb7395
elenuki33 <elenuki333@gmail.com>
2021-05-22 16:17:52 +0200

    User controller

---
+++ esports-angular/src/main/java/hateoas/UserRepresentationModelAssembler.java
@@ -0,0 +1,35 @@
+package hateoas;
+import cz.muni.fi.pa165.esports.dto.CompetitionDTO;
+import cz.muni.fi.pa165.esports.dto.PlayerDTO;
+import cz.muni.fi.pa165.esports.dto.UserDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+
+@Slf4j
+@Component
+public class UserRepresentationModelAssembler implements RepresentationModelAssembler<UserDTO, EntityModel<UserDTO>> {
+    @Inject
+    private EntityLinks entityLinks;
+
+    @Override
+    public EntityModel<UserDTO> toModel(UserDTO entity) {
+        long id = entity.getId();
+        EntityModel<UserDTO> userResourceModel = new EntityModel<>(entity);
+        try {
+            Link catLink = entityLinks.linkForItemResource(UserDTO.class, id).withSelfRel();
+            userResourceModel.add(catLink);
+
+            //Link playerLink = entityLinks.linkForItemResource(CompetitionDTO.class, id).slash("/competitions").withRel("competitions");
+        } catch (Exception e) {
+            log.error("cannot link HATEOAS: {}", e.getMessage());
+        }
+        return userResourceModel;
+    }
+}
