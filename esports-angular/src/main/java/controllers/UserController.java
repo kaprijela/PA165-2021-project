efe2299db4421efcd8668277aabaa7b43090a11e
Gabriela Kandová <gabika.kandova@gmail.com>
2021-05-23 16:33:39 +0200

    Added dummy user controller

--- esports-angular/src/main/java/controllers/UserController.java
+++ esports-angular/src/main/java/controllers/UserController.java
@@ -5,6 +5,7 @@ import cz.muni.fi.pa165.esports.dto.CompetitionDTO;
 import cz.muni.fi.pa165.esports.dto.PlayerDTO;
 import cz.muni.fi.pa165.esports.dto.TeamDTO;
 import cz.muni.fi.pa165.esports.dto.UserDTO;
+import cz.muni.fi.pa165.esports.enums.Role;
 import cz.muni.fi.pa165.esports.facade.CompetitionFacade;
 import cz.muni.fi.pa165.esports.facade.PlayerFacade;
 import cz.muni.fi.pa165.esports.facade.TeamFacade;
@@ -30,6 +31,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -42,9 +45,18 @@ public class UserController {
     UserFacade userFacade;
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public boolean login(@RequestBody AuthenticatedUserDTO user) {
+    public List<Role> login(@RequestBody AuthenticatedUserDTO user) {
         log.info(user.getUsername(), user.getPassword());
-        return "admin".equals(user.getUsername()) && "admin".equals(user.getPassword());
+        if ("teamAdmin".equals(user.getUsername()) && "admin".equals(user.getPassword())) {
+            return Arrays.asList(Role.TEAM_MANAGER, Role.PLAYER);
+        }
+        if ("compAdmin".equals(user.getUsername()) && "admin".equals(user.getPassword())) {
+            return Collections.singletonList(Role.COMPETITION_MANAGER);
+        }
+        if ("player".equals(user.getUsername()) && "player".equals(user.getPassword())) {
+            return Collections.singletonList(Role.PLAYER);
+        }
+        return null;
     }
 /*
     @RequestMapping(value = "", method = RequestMethod.GET)
