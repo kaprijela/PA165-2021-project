3f86ba980e41b516ce45cef3875b9d57d07a62b2
Jan Gavlik <jan.gavlik@tietoevry.com>
2021-05-23 18:39:47 +0200

    add detail

---
+++ esports-angular-new/src/app/components/player-detail/player-detail.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {Player} from "../../model/player";
+import {PlayerService} from "../../service/player.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Statistics} from "../../model/statistics";
+
+@Component({
+  selector: 'app-player-detail',
+  templateUrl: './player-detail.component.html',
+  styleUrls: ['./player-detail.component.css']
+})
+export class PlayerDetailComponent implements OnInit {
+  player?: Player
+  statistics?: Statistics
+
+  constructor(private playerService: PlayerService,
+              private router: Router,
+              private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    const param = this.route.snapshot.paramMap.get("id");
+    if (param) {
+      const id = +param;
+      this.getPlayer(id);
+    }
+  }
+
+  private getPlayer(id: number) {
+    console.log("getPlayer")
+    this.playerService.findById(id).subscribe(data => {
+      this.player = data;
+    })
+  }
+
+  private getStatsPlayer(id: number) {
+    console.log("getPlayer")
+    this.playerService.findById(id).subscribe(data => {
+      this.player = data;
+    })
+  }
+
+}
