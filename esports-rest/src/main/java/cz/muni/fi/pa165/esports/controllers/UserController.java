bd81be3ec4c86780f9903cff50927c57455ab7f2
Gabriela Kandová <gabika.kandova@gmail.com>
2021-06-06 16:11:58 +0200

    User controller edits

--- esports-rest/src/main/java/cz/muni/fi/pa165/esports/controllers/UserController.java
+++ esports-rest/src/main/java/cz/muni/fi/pa165/esports/controllers/UserController.java
@@ -1,9 +1,11 @@
 package cz.muni.fi.pa165.esports.controllers;
 
 import cz.muni.fi.pa165.esports.dto.AuthenticatedUserDTO;
+import cz.muni.fi.pa165.esports.dto.UserDTO;
 import cz.muni.fi.pa165.esports.enums.Role;
 import cz.muni.fi.pa165.esports.facade.UserFacade;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
@@ -12,13 +14,17 @@ import java.util.Collections;
 import java.util.List;
 
 @Slf4j
-@RestController
 @CrossOrigin
+@RestController
 public class UserController {
-    @Inject
-    UserFacade userFacade;
+    private final UserFacade userFacade;
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @Inject
+    public UserController(UserFacade userFacade) {
+        this.userFacade = userFacade;
+    }
+
+    @PostMapping(value = "/login")
     public List<Role> login(@RequestBody AuthenticatedUserDTO user) {
         log.info(user.getUsername(), user.getPassword());
         if ("teamAdmin".equals(user.getUsername()) && "admin".equals(user.getPassword())) {
@@ -32,14 +38,14 @@ public class UserController {
         }
         return null;
     }
-/*
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public final List<UserDTO> getUsers() {
-        log.debug("restv2 getUsers()");
+
+    @GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
+    public final List<UserDTO> getAllUsers() {
+        log.debug("REST getAllUsers()");
         return userFacade.getAllUsers();
     }
 
-
+    /*
     @RequestMapping(value = "/name/{name}", method = RequestMethod.GET)
     public final List<UserDTO> getByName(@PathVariable("name") String name) throws Exception {
         log.debug("restv2 get by name {}", name);
