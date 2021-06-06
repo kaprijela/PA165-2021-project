035a3dab5ef38814af004c652f0e3a65f2acb0d9
Gabriela Kandová <gabika.kandova@gmail.com>
2021-06-06 16:39:50 +0200

    Added get user by ID method

--- esports-rest/src/main/java/cz/muni/fi/pa165/esports/controllers/UserController.java
+++ esports-rest/src/main/java/cz/muni/fi/pa165/esports/controllers/UserController.java
@@ -4,14 +4,13 @@ import cz.muni.fi.pa165.esports.dto.AuthenticatedUserDTO;
 import cz.muni.fi.pa165.esports.dto.UserDTO;
 import cz.muni.fi.pa165.esports.enums.Role;
 import cz.muni.fi.pa165.esports.exceptions.InvalidRequestException;
+import cz.muni.fi.pa165.esports.exceptions.ResourceNotFoundException;
 import cz.muni.fi.pa165.esports.facade.UserFacade;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Slf4j
@@ -39,29 +38,15 @@ public class UserController {
         return userFacade.getAllUsers();
     }
 
-    /*
-    @RequestMapping(value = "/name/{name}", method = RequestMethod.GET)
-    public final List<UserDTO> getByName(@PathVariable("name") String name) throws Exception {
-        log.debug("restv2 get by name {}", name);
-
-        UserDTO userByName = userFacade.findByUsername(name);
-        if (userByName == null) {
-            throw new ResourceNotFoundException("User not found");
-        }
-        log.debug("restv2 getPlayers()");
-        return userFacade.getAllUsers();
-    }
-
-    @RequestMapping(value = "/id/{id}", method = RequestMethod.GET)
-    public final UserDTO getById(@PathVariable("id") Long id) throws Exception {
-        log.debug("restv2 get by id {}", id);
+    @GetMapping(value = "/users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public final UserDTO getUserById(@PathVariable("id") Long id) throws ResourceNotFoundException {
+        log.debug("REST getUserById({})", id);
 
         UserDTO userById = userFacade.findById(id);
         if (userById == null) {
-            throw new ResourceNotFoundException("PLayer not found");
+            throw new ResourceNotFoundException("User not found");
         }
         return userById;
     }
- */
 }
 
